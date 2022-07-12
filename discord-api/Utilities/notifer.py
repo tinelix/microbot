ed6a08dd9508b38400789d2fa9bbb723bd2b5057
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2022-07-12 23:25:05 +0700

    added new function (refreshing status)

--- discord-api/Utilities/notifer.py
+++ discord-api/Utilities/notifer.py
@@ -4,3 +4,7 @@ async def showWelcomeMessage(disnake, bot, config):
           ' L: {3} ms │ G: {4}'.format(config['name'], config['version'], '{0}#{1}'.format(bot.user.name, bot.user.discriminator), round(bot.latency * 1000, 2), len(bot.guilds)))
     game = disnake.Game("{0} guilds".format(len(bot.guilds)), type=disnake.ActivityType.watching)
     await bot.change_presence(status=disnake.Status.dnd, activity=game)
+
+async def refreshStatus(disnake, bot, config):
+    game = disnake.Game("{0} guilds".format(len(bot.guilds)), type=disnake.ActivityType.watching)
+    await bot.change_presence(status=disnake.Status.dnd, activity=game)
