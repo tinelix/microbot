72624e3400d111609473248bb0be56e45adad7f7
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2022-08-21 23:36:00 +0700

    ok, i forgot help command specify

--- discord-api/Utilities/notifier.py
+++ discord-api/Utilities/notifier.py
@@ -15,7 +15,7 @@ async def showWelcomeMessage(disnake, bot, config):
     print('\n {0} {1}\n Connected to {2}!\n Copyright © 2022 Dmitry Tretyakov (aka. Tinelix)'
           '\n ─────────────────────────────────────────────────────── \n'
           ' L: {3} ms │ G: {4}\n ───────────────────────────────────────────────────────'.format(config['name'], config['version'], '{0}#{1}'.format(bot.user.name, bot.user.discriminator), round(bot.latency * 1000, 2), len(bot.guilds)))
-    statuses = ["{0} guilds".format(len(bot.guilds)), "Type {0} for help".format(config['prefix']), "Version {0}".format(config['version'])]
+    statuses = ["{0} guilds".format(len(bot.guilds)), "Type {0}help for help".format(config['prefix']), "Version {0}".format(config['version'])]
     statuses_cycle = cycle(statuses)
     game = disnake.Game(statuses[0], type=disnake.ActivityType.watching)
     await bot.change_presence(status=disnake.Status.dnd, activity=game)
