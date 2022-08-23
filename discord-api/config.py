2012b3ff6280805b774136a49d55b099ad6d7602
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2022-08-23 23:27:29 +0700

    Add files via upload

--- discord-api/config.py
+++ discord-api/config.py
@@ -20,7 +20,7 @@ config = {
     'accent_err': 0xff4444,
     'dev_id': 741883312108339231, # Bot developer ID
     'bugs_ch': 995275176029732894,  # To show a bug report in the console, set the value to 0
-    'cooldown': 0.8
+    'cooldown': 1.5
 }
 
 links = {
