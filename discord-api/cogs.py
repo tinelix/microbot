18a939b265fc0caaf37b1aa976c0d52e4401e940
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-10-14 23:19:54 +0300

    Update cogs.py

--- discord-api/cogs.py
+++ discord-api/cogs.py
@@ -37,17 +37,17 @@ class Commands(commands.Cog):
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def help_cmd(self, ctx, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await help.sendCmdHelpMsg(ctx, self.bot, links, config, language, disnake, translator, arg)
 
     @commands.slash_command(name="help", description=translator.translate('command_description', 'help', 'en_US'))
     async def help_scmd(self, ctx):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await help.sendSlashMsg(ctx, self.bot, config, links, version, language, disnake, translator)
 
     @commands.command(name="about", description=translator.translate('command_description', 'about', 'en_US'), aliases=['state', 'check'])
@@ -55,9 +55,9 @@ class Commands(commands.Cog):
     async def about_cmd(self, ctx):
         uptime = str(datetime.timedelta(seconds=int(round(time.time() - self.connectionStartTime))))
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await about.sendRegularMsg(ctx, self.bot, config, links, language, disnake, translator, python_version, uptime, self.tz, version)
 
     @commands.slash_command(name="about", description=translator.translate('command_description', 'about', 'en_US'))
@@ -65,156 +65,156 @@ class Commands(commands.Cog):
     async def about_scmd(self, ctx):
         uptime = str(datetime.timedelta(seconds=int(round(time.time() - self.connectionStartTime))))
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await about.sendSlashMsg(ctx, self.bot, config, links, language, disnake, translator, python_version, uptime, self.tz)
 
     @commands.command(name="user", description=translator.translate('command_description', 'user', 'en_US'), aliases=['member'])
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def user_cmd(self, ctx, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await user.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg, self.tz)
 
     @commands.slash_command(name="user", description=translator.translate('command_description', 'user', 'en_US'))
     async def user_scmd(self, ctx, member):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await user.sendSlashMsg(ctx, self.bot, config, language, disnake, translator, member, self.tz)
 
     @commands.command(name="avatar", description=translator.translate('command_description', 'avatar', 'en_US'))
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def avatar_cmd(self, ctx, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await avatar.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg)
 
     @commands.slash_command(name="avatar", description=translator.translate('command_description', 'avatar', 'en_US'))
     async def avatar_scmd(self, ctx, member):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await avatar.sendSlashMsg(ctx, self.bot, config, language, disnake, translator, member)
 
     @commands.command(name="8ball", description=translator.translate('command_description', '8ball', 'en_US'))
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def eightball_cmd(self, ctx, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await eightball.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg)
 
     @commands.slash_command(name="8ball", description=translator.translate('command_description', '8ball', 'en_US'))
     async def eightball_scmd(self, ctx, question):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await eightball.sendSlashMsg(ctx, self.bot, config, language, disnake, translator, question)
 
     @commands.command(name="rngen", description=translator.translate('command_description', 'rngen', 'en_US'), aliases=['rand'])
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def rngen_cmd(self, ctx, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await rngen.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg)
 
     @commands.slash_command(name="rngen", description=translator.translate('command_description', 'rngen', 'en_US'))
     async def rngen_scmd(self, ctx, range):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await rngen.sendSlashMsg(ctx, self.bot, config, language, disnake, translator, range)
 
     @commands.command(name="eval")
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def eval_cmd(self, ctx, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await eval.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg)
 
     @commands.command(name="test")
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def eval_cmd(self, ctx, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await test.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg, fatalerr_reporter)
 
     @commands.command(name="guild", description=translator.translate('command_description', 'guild', 'en_US'), aliases=['server'])
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def guild_cmd(self, ctx):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await guild.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, self.tz)
 
     @commands.slash_command(name="guild", description=translator.translate('command_description', 'guild', 'en_US'))
     async def guild_scmd(self, ctx):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await guild.sendSlashMsg(ctx, self.bot, config, language, disnake, translator, self.tz)
 
     @commands.command(name="calc", description=translator.translate('command_description', 'calc', 'en_US'))
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def calc_cmd(self, ctx, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await calc.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg)
 
     @commands.slash_command(name="calc", description=translator.translate('command_description', 'calc', 'en_US'))
     async def calc_scmd(self, ctx, expression):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await calc.sendSlashMsg(ctx, self.bot, config, language, disnake, translator, expression)
 
     @commands.command(name="settings", description=translator.translate('command_description', 'settings', 'en_US'))
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def settings_cmd(self, ctx, *arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await settings.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg, db, self.bot.database, self.bot.cursor, guild_data, user_data)
 
     @commands.command(name="publish", description=translator.translate('command_description', 'publish', 'en_US'), aliases=['post'])
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def publish_cmd(self, ctx, *, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         now = datetime.datetime.now(datetime.timezone.utc).astimezone()
         await publish.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg)
 
     @commands.slash_command(name="publish", description=translator.translate('command_description', 'publish', 'en_US'), aliases=['post'])
     async def publish_scmd(self, ctx, *, text):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         now = datetime.datetime.now(datetime.timezone.utc).astimezone()
         await publish.sendSlashMsg(ctx, self.bot, config, language, disnake, translator, text)
 
@@ -222,60 +222,60 @@ class Commands(commands.Cog):
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def ping_cmd(self, ctx):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await ping.sendRegularMsg(ctx, self.bot, config, language, disnake, translator)
 
     @commands.slash_command(name="ping", description=translator.translate('command_description', 'ping', 'en_US'))
     async def ping_scmd(self, ctx):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await ping.sendSlashMsg(ctx, self.bot, config, language, disnake, translator)
 
     @commands.command(name="weather", description=translator.translate('command_description', 'weather', 'en_US'))
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def weather_cmd(self, ctx, *, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await weather.sendRegularMsg(ctx, self.bot, config, tokens, language, disnake, translator, arg)
 
     @commands.slash_command(name="weather", description=translator.translate('command_description', 'weather2', 'en_US'))
     async def weather_scmd(self, ctx, *, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await weather.sendSlashMsg(ctx, self.bot, config, tokens, language, disnake, translator, arg)
 
     @commands.command(name="wiki", description=translator.translate('command_description', 'wiki', 'en_US'))
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def wiki_cmd(self, ctx, *, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await wiki.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg)
 
     @commands.slash_command(name="wiki", description=translator.translate('command_description', 'wiki', 'en_US'))
     async def wiki_scmd(self, ctx, *, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await wiki.sendSlashMsg(ctx, self.bot, config, language, disnake, translator, arg)
 
     @commands.command(name="codec", description=translator.translate('command_description', 'codec', 'en_US'))
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def codec_cmd(self, ctx, *arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         now = datetime.datetime.now(datetime.timezone.utc).astimezone()
         await codec.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg, binary)
 
@@ -283,9 +283,9 @@ class Commands(commands.Cog):
     @commands.cooldown(1, config['cooldown'], commands.BucketType.user)
     async def timers_cmd(self, ctx, *, arg):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         await timers.sendRegularMsg(ctx, self.bot, config, language, disnake, translator, arg, db, self.bot.database, self.bot.cursor, self.tz)
 
 class Listeners(commands.Cog):
@@ -319,10 +319,10 @@ class Listeners(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'regular')
-        language = guild_data[1]
+        language = guild_data[2]
         custom_prefix = ''
         user_data = await sync_db(self.bot, ctx, 'users', 'regular')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         for prefix in await self.bot.get_prefix(ctx.message):
             if(ctx.message.content.startswith(prefix)):
                 custom_prefix = prefix
@@ -348,10 +348,10 @@ class Listeners(commands.Cog):
     @commands.Cog.listener()
     async def on_slash_command_error(self, ctx, error):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
-        language = guild_data[1]
+        language = guild_data[2]
         custom_prefix = ''
         user_data = await sync_db(self.bot, ctx, 'users', 'slash')
-        self.tz = pytz.timezone(user_data[4])
+        self.tz = pytz.timezone(user_data[5])
         error_list = []
         error_text = "".join(traceback.TracebackException.from_exception(error).format())
         if(config['bugs_ch'] > 0):
