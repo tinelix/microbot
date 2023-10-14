c48a51067045f0c0d67020ab87eebd0e4e5d9791
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-10-14 23:12:26 +0300

    Update notifier.py

--- discord-api/Utilities/notifier.py
+++ discord-api/Utilities/notifier.py
@@ -6,15 +6,33 @@ import os
 from disnake.ext import tasks
 from itertools import cycle
 
+async def getConnectionQualityPseudoGraph(bot):
+    if(round(bot.latency * 1000, 2) > 2000):
+        connection_quality = "{0} ms [\033[91m....\033[0m]".format(round(bot.latency * 1000, 2))
+    else if(round(bot.latency * 1000, 2) > 1500):
+        connection_quality = "{0} ms [\033[91m|...\033[0m]".format(round(bot.latency * 1000, 2))
+    else if(round(bot.latency * 1000, 2) > 1000):
+        connection_quality = "{0} ms [\033[93m||..\033[0m]".format(round(bot.latency * 1000, 2))
+    else if(round(bot.latency * 1000, 2) > 500):
+        connection_quality = "{0} ms [\033[92m|||.\033[0m]".format(round(bot.latency * 1000, 2))
+    else:
+        connection_quality = "{0} ms [\033[92m||||\033[0m]".format(round(bot.latency * 1000, 2))
+
+    return connection_quality
+
 async def showWelcomeMessage(disnake, bot, config, version):
     if(os.name == 'nt'):
         clear_cmd = 'cls'
     else:
         clear_cmd = 'clear'
     os.system(clear_cmd)
-    print('\n {0} {1}\n Connected to {2}!\n Copyright © 2023 Dmitry Tretyakov (aka. Tinelix)'
+
+    print('\n {0} {1}\n\033[92m Connected to {2}!\033[0m\n Copyright © 2023 Dmitry Tretyakov (aka. Tinelix)'
           '\n ─────────────────────────────────────────────────────── \n'
-          ' L: {3} ms │ G: {4}\n ───────────────────────────────────────────────────────'.format(version['original_name'], version['version'], '{0} ({1})'.format(bot.user.name, bot.user.global_name), round(bot.latency * 1000, 2), len(bot.guilds)))
+          ' L: {3} │ G: {4}'
+          '\n ───────────────────────────────────────────────────────'
+          .format(version['original_name'], version['version'], '{0} ({1})'
+                  .format(bot.user.name, bot.user.global_name), getConnectionQualityPseudoGraph(bot), len(bot.guilds)))
     statuses = ["{0} guilds".format(len(bot.guilds)), "{0}help".format(config['prefix']), "Version {0}".format(version['version'])]
     statuses_cycle = cycle(statuses)
     game = disnake.Game(statuses[0], type=disnake.ActivityType.watching)
@@ -33,4 +51,6 @@ async def updateWelcomeMessage(disnake, bot, config, versiom):
     os.system(clear_cmd)
     print('\n {0} {1}\n Connected to {2}!\n Copyright © 2023 Dmitry Tretyakov (aka. Tinelix)'
           '\n ─────────────────────────────────────────────────────── \n'
-          ' L: {3} ms │ G: {4}\n ───────────────────────────────────────────────────────'.format(version['original_name'], version['version'], '{0} ({1})'.format(bot.user.name, bot.user.global_name), round(bot.latency * 1000, 2), len(bot.guilds)))
+          ' L: {3} ms │ G: {4}\n ───────────────────────────────────────────────────────'
+          .format(version['original_name'], version['version'], '{0} ({1})'
+                  .format(bot.user.name, bot.user.global_name), getConnectionQualityPseudoGraph(bot), len(bot.guilds)))
