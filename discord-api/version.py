f977ec9d7df609d80019b33c4e5c5e03d548279d
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-10-15 18:09:49 +0300

    Version 0.8.1

--- discord-api/version.py
+++ discord-api/version.py
@@ -16,6 +16,6 @@
 version = {
     'original_name': 'Microbot',
     'original_repo': 'https://github.com/tinelix/microbot',
-    'version': '0.8.0',
+    'version': '0.8.1',
     'version_date': '2023-10-15'
 }
