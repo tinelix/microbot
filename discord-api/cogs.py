146bc745e02d063bec652b1f1d80a8b9076e475f
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2023-10-15 17:00:12 +0300

    Update cogs.py

--- discord-api/cogs.py
+++ discord-api/cogs.py
@@ -214,9 +214,9 @@ class Commands(commands.Cog):
 
     @commands.slash_command(name="settings", description=translator.translate('command_description', 'settings', 'en_US'))
     async def settings_cmd(self, ctx,
-                           language: Option(str, desciption="Language", required=False),
-                           prefix: Option(str, desciption="Prefix", required=False),
-                           timezone: Option(str, desciption="Timezone", required=False)
+                           language: disnake.Option(str, desciption="Language", required=False),
+                           prefix: disnake.Option(str, desciption="Prefix", required=False),
+                           timezone: disnake.Option(str, desciption="Timezone", required=False)
                           ):
         guild_data = await sync_db(self.bot, ctx, 'guilds', 'slash')
         language = guild_data[2]
