b979eb20d03850519074ea891f794d2d05adf460
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-01-01 19:35:15 +0700

    Update weather.py

--- discord-api/Commands/weather.py
+++ discord-api/Commands/weather.py
@@ -21,7 +21,7 @@ hidden = False
 
 cities_list = []
 
-async def generateEmbed(ctx, bot, config, tokens, language, disnake, translator, arg):
+async def generateEmbed(ctx, inst, config, tokens, disnake, translator, arg):
     if(len(tokens['owm_api']) == 0):
         return print(" ERROR: OpenWeatherMap API token not found.")
     data = requests.get("http://api.openweathermap.org/data/2.5/find",
@@ -36,83 +36,83 @@ async def generateEmbed(ctx, bot, config, tokens, language, disnake, translator,
         msg_embed = disnake.Embed(
             colour=config['accent_def'],
         )
-        msg_embed.set_author(name=str(translator.translate('embed_title', 'weather', language)))
-        msg_embed.add_field(translator.translate('embed_fields', 'weather_resultf', language).format(len(cities_list)), translator.translate('embed_fields', 'weather_resultv', language).format(cities))
+        msg_embed.set_author(name=str(translator.translate('embed_title', 'weather', inst.language)))
+        msg_embed.add_field(translator.translate('embed_fields', 'weather_resultf', inst.language).format(len(cities_list)), translator.translate('embed_fields', 'weather_resultv', inst.language).format(cities))
     else:
         msg_embed = disnake.Embed(
             colour=config['accent_err'],
-            description=translator.translate('embed_description', 'query_notfound', language)
+            description=translator.translate('embed_description', 'query_notfound', inst.language)
         )
-        msg_embed.set_author(name=str(translator.translate('embed_title', 'error', language)))
+        msg_embed.set_author(name=str(translator.translate('embed_title', 'error', inst.language)))
     return msg_embed
 
 def getConditionsDescription(code, translator, language):
     conditions = ""
     if(code == 200):
-        conditions = translator.translate('embed_description', 'weather_conditions_200', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_200', inst.language)
     elif(code == 201):
-        conditions = translator.translate('embed_description', 'weather_conditions_201', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_201', inst.language)
     elif(code == 202):
-        conditions = translator.translate('embed_description', 'weather_conditions_202', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_202', inst.language)
     elif(code >= 300 and code <= 321):
-        conditions = translator.translate('embed_description', 'weather_conditions_300-321', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_300-321', inst.language)
     elif(code >= 500 and code <= 501):
-        conditions = translator.translate('embed_description', 'weather_conditions_500-501', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_500-501', inst.language)
     elif(code == 502):
-        conditions = translator.translate('embed_description', 'weather_conditions_502', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_502', inst.language)
     elif(code >= 503 and code <= 504):
-        conditions = translator.translate('embed_description', 'weather_conditions_503-504', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_503-504', inst.language)
     elif(code == 511):
-        conditions = translator.translate('embed_description', 'weather_conditions_511', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_511', inst.language)
     elif(code >= 600 and code <= 601):
-        conditions = translator.translate('embed_description', 'weather_conditions_600-601', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_600-601', inst.language)
     elif(code == 602):
-        conditions = translator.translate('embed_description', 'weather_conditions_602', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_602', inst.language)
     elif(code >= 615 and code <= 616):
-        conditions = translator.translate('embed_description', 'weather_conditions_615-616', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_615-616', inst.language)
     elif(code == 800):
-        conditions = translator.translate('embed_description', 'weather_conditions_800', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_800', inst.language)
     elif(code == 801):
-        conditions = translator.translate('embed_description', 'weather_conditions_801', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_801', inst.language)
     elif(code >= 802 and code <= 803):
-        conditions = translator.translate('embed_description', 'weather_conditions_802-803', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_802-803', inst.language)
     elif(code == 804):
-        conditions = translator.translate('embed_description', 'weather_conditions_804', language)
+        conditions = translator.translate('embed_description', 'weather_conditions_804', inst.language)
 
     return conditions
 
 def getConditionsShortDescription(code, translator, language):
     conditions = ""
     if(code == 200):
-        conditions = translator.translate('embed_description', 'weather_sconditions_200', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_200', inst.language)
     elif(code == 201):
-        conditions = translator.translate('embed_description', 'weather_sconditions_201', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_201', inst.language)
     elif(code == 202):
-        conditions = translator.translate('embed_description', 'weather_sconditions_202', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_202', inst.language)
     elif(code >= 300 and code <= 321):
-        conditions = translator.translate('embed_description', 'weather_sconditions_300-321', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_300-321', inst.language)
     elif(code >= 500 and code <= 501):
-        conditions = translator.translate('embed_description', 'weather_sconditions_500-501', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_500-501', inst.language)
     elif(code == 502):
-        conditions = translator.translate('embed_description', 'weather_sconditions_502', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_502', inst.language)
     elif(code >= 503 and code <= 504):
-        conditions = translator.translate('embed_description', 'weather_sconditions_503-504', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_503-504', inst.language)
     elif(code == 511):
-        conditions = translator.translate('embed_description', 'weather_sconditions_511', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_511', inst.language)
     elif(code >= 600 and code <= 601):
-        conditions = translator.translate('embed_description', 'weather_sconditions_600-601', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_600-601', inst.language)
     elif(code == 602):
-        conditions = translator.translate('embed_description', 'weather_sconditions_602', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_602', inst.language)
     elif(code >= 615 and code <= 616):
-        conditions = translator.translate('embed_description', 'weather_sconditions_615-616', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_615-616', inst.language)
     elif(code == 800):
-        conditions = translator.translate('embed_description', 'weather_sconditions_800', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_800', inst.language)
     elif(code == 801):
-        conditions = translator.translate('embed_description', 'weather_sconditions_801', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_801', inst.language)
     elif(code >= 802 and code <= 803):
-        conditions = translator.translate('embed_description', 'weather_sconditions_802-803', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_802-803', inst.language)
     elif(code == 804):
-        conditions = translator.translate('embed_description', 'weather_sconditions_804', language)
+        conditions = translator.translate('embed_description', 'weather_sconditions_804', inst.language)
 
     return conditions
 def formatTemperature(temp):
@@ -125,10 +125,10 @@ def formatTemperature(temp):
 
     return ftemp
 
-async def generateWeatherEmbed(ctx, bot, config, tokens, language, disnake, translator, city_id):
+async def generateWeatherEmbed(ctx, inst, config, tokens, disnake, translator, city_id):
     if(len(tokens['owm_api']) == 0):
         return print(" ERROR: OpenWeatherMap API token not found.")
-    if(language == "ru_RU"):
+    if(inst.language == "ru_RU"):
         data = requests.get("http://api.openweathermap.org/data/2.5/weather",
                         params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': tokens['owm_api']})
         forecast_data = requests.get("http://api.openweathermap.org/data/2.5/forecast",
@@ -157,31 +157,31 @@ async def generateWeatherEmbed(ctx, bot, config, tokens, language, disnake, tran
 
     for day in forecast['list']:
         if(forecast['list'].index(day) < 7):
-            conditions = getConditionsShortDescription(day['weather'][0]['id'], translator, language)
+            conditions = getConditionsShortDescription(day['weather'][0]['id'], translator, inst.language)
             temp = formatTemperature(day['main']['temp'])
             min_temp = formatTemperature(day['main']['temp_min'])
             max_temp = formatTemperature(day['main']['temp_max'])
             day7_forecast += "{0} | {1}°C\r\n{2}\r\n".format(day['dt_txt'], temp, conditions)
 
     msg_embed.set_author(name=str(translator.translate('embed_title', 'weather2', language)).format(weather['name'], weather['sys']['country']))
-    msg_embed.add_field(translator.translate('embed_fields', 'weather_tempf', language), translator.translate('embed_fields', 'weather_tempv', language).format(current_temp, current_min_temp, current_max_temp), inline=False)
-    msg_embed.add_field(translator.translate('embed_fields', 'weather_pressuref', language), translator.translate('embed_fields', 'weather_pressurev', language).format(round(weather['main']['pressure'] * 0.75006)))
-    msg_embed.add_field(translator.translate('embed_fields', 'weather_humidityf', language), translator.translate('embed_fields', 'weather_humidityv', language).format(weather['main']['humidity']))
-    msg_embed.add_field(translator.translate('embed_fields', 'weather_windspeedf', language), translator.translate('embed_fields', 'weather_windspeedv', language).format(round(weather['wind']['speed'], 1)))
-    msg_embed.add_field(translator.translate('embed_fields', 'weather_upforecastsf', language), translator.translate('embed_fields', 'weather_upforecastsv', language).format(day7_forecast), inline=False)
+    msg_embed.add_field(translator.translate('embed_fields', 'weather_tempf', inst.language), translator.translate('embed_fields', 'weather_tempv', inst.language).format(current_temp, current_min_temp, current_max_temp), inline=False)
+    msg_embed.add_field(translator.translate('embed_fields', 'weather_pressuref', inst.language), translator.translate('embed_fields', 'weather_pressurev', inst.language).format(round(weather['main']['pressure'] * 0.75006)))
+    msg_embed.add_field(translator.translate('embed_fields', 'weather_humidityf', inst.language), translator.translate('embed_fields', 'weather_humidityv', inst.language).format(weather['main']['humidity']))
+    msg_embed.add_field(translator.translate('embed_fields', 'weather_windspeedf', inst.language), translator.translate('embed_fields', 'weather_windspeedv', inst.language).format(round(weather['wind']['speed'], 1)))
+    msg_embed.add_field(translator.translate('embed_fields', 'weather_upforecastsf', inst.language), translator.translate('embed_fields', 'weather_upforecastsv', inst.language).format(day7_forecast), inline=False)
     return msg_embed
 
-async def sendSlashMsg(ctx, bot, config, tokens, language, disnake, translator, arg):
-    msg_embed = await generateEmbed(ctx, bot, config, tokens, language, disnake, translator, arg)
+async def sendSlashMsg(ctx, inst, config, tokens, disnake, translator, arg):
+    msg_embed = await generateEmbed(ctx, inst, config, tokens, disnake, translator, arg)
     if(len(cities_list) > 0):
         select = disnake.ui.Select(
-                    placeholder=translator.translate('embed_fields', 'weather_selyc', language),
+                    placeholder=translator.translate('embed_fields', 'weather_selyc', inst.language),
                     options=cities_list,
                 )
         async def response(interaction):
             id = int(re.search(r'\d+', select.values[0]).group())
             if(id > 0):
-                msg_embed = await generateWeatherEmbed(ctx, bot, config, tokens, language, disnake, translator, id)
+                msg_embed = await generateWeatherEmbed(ctx, bot, config, tokens, inst.language, disnake, translator, id)
                 await interaction.send(embed=msg_embed)
 
         select.callback = response
@@ -193,17 +193,17 @@ async def sendSlashMsg(ctx, bot, config, tokens, language, disnake, translator, 
     else:
         await ctx.response.send_message(embed=msg_embed)
 
-async def sendRegularMsg(ctx, bot, config, tokens, language, disnake, translator, arg):
-    msg_embed = await generateEmbed(ctx, bot, config, tokens, language, disnake, translator, arg)
+async def sendRegularMsg(ctx, inst, config, tokens, disnake, translator, arg):
+    msg_embed = await generateEmbed(ctx, inst, config, tokens, disnake, translator, arg)
     if(len(cities_list) > 0):
         select = disnake.ui.Select(
-                    placeholder=translator.translate('embed_fields', 'weather_selyc', language),
+                    placeholder=translator.translate('embed_fields', 'weather_selyc', inst.language),
                     options=cities_list,
                 )
         async def response(interaction):
             id = int(re.search(r'\d+', select.values[0]).group())
             if(id > 0):
-                msg_embed = await generateWeatherEmbed(ctx, bot, config, tokens, language, disnake, translator, id)
+                msg_embed = await generateWeatherEmbed(ctx, inst, config, tokens, disnake, translator, id)
                 await interaction.send(embed=msg_embed)
 
         select.callback = response
@@ -217,9 +217,9 @@ async def sendRegularMsg(ctx, bot, config, tokens, language, disnake, translator
 
 async def sendHelpMsg(ctx, bot, config, language, disnake, translator):
     msg_embed = disnake.Embed(
-        title=str(translator.translate('embed_title', 'cmd_help', language)).format('weather'),
-        description=str(translator.translate('command_description', 'weather', language)),
+        title=str(translator.translate('embed_title', 'cmd_help', inst.language)).format('weather'),
+        description=str(translator.translate('command_description', 'weather', inst.language)),
         colour=config['accent_def'],
     )
-    msg_embed.add_field(translator.translate('embed_fields', 'help_exampf', language), translator.translate('command_examples', 'weather', language).format(config['prefix']), inline=False)
+    msg_embed.add_field(translator.translate('embed_fields', 'help_exampf', inst.language), translator.translate('command_examples', 'weather', inst.language).format(config['prefix']), inline=False)
     await ctx.reply(embed=msg_embed, mention_author=False)
