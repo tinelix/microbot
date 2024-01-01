7c492931ffef41a764f42430760f1c247b420e15
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-01-01 18:07:18 +0700

    Update eval.py

--- discord-api/Commands/eval.py
+++ discord-api/Commands/eval.py
@@ -20,13 +20,13 @@ from datetime import datetime
 name = 'eval'
 hidden = True
 
-async def generateEmbed(ctx, bot, config, language, disnake, translator, arg):
+async def generateEmbed(ctx, inst, config, disnake, translator, arg):
     if(ctx.message.author.id == config['dev_id']):  # only bot owner!
         try:
             msg_embed = disnake.Embed(
                 colour=config['accent_def'],
             )
-            msg_embed.set_author(name=str(translator.translate('embed_title', 'eval', language)))
+            msg_embed.set_author(name=str(translator.translate('embed_title', 'eval', inst.language)))
             if(arg == 'guilds'): # guild list for statistical data
                 eval_r = ''
                 for guild in bot.guilds:
@@ -34,7 +34,7 @@ async def generateEmbed(ctx, bot, config, language, disnake, translator, arg):
                     for member in guild.members:
                         if(str(member.status) == 'online' or str(member.status) == 'idle' or str(member.status) == 'dnd'):
                             online += 1
-                    eval_r += '{0:03d}. {1}\r\n└────┤ {2: 4d} members │ {3: 4d} online │\r\n'.format(bot.guilds.index(guild) + 1, guild.name, guild.member_count, online)
+                    eval_r += '{0:03d}. {1}\r\n└────┤ {2: 4d} members │ {3: 4d} online │\r\n'.format(inst.bot.guilds.index(guild) + 1, guild.name, guild.member_count, online)
             elif("token" in arg):
                 eval_r = 'https://www.youtube.com/watch?v=2bXP5VNukmY'
             else:
@@ -45,24 +45,24 @@ async def generateEmbed(ctx, bot, config, language, disnake, translator, arg):
             if(arg == 'guilds'):
                 pass
             else:
-                msg_embed.add_field(translator.translate('embed_fields', 'eval_codelf', language), '```py\r\n{0}```'.format(arg), inline=False)
-            msg_embed.add_field(translator.translate('embed_fields', 'eval_resulf', language), '```py\r\n{0}```'.format(eval_r), inline=False)
+                msg_embed.add_field(translator.translate('embed_fields', 'eval_codelf', inst.language), '```py\r\n{0}```'.format(arg), inline=False)
+            msg_embed.add_field(translator.translate('embed_fields', 'eval_resulf', inst.language), '```py\r\n{0}```'.format(eval_r), inline=False)
         except Exception as e:
             msg_embed = disnake.Embed(
                 colour=config['accent_err'],
             )
-            msg_embed.set_author(name=str(translator.translate('embed_title', 'eval', language)))
-            msg_embed.add_field(translator.translate('embed_fields', 'eval_resulf', language), '```py\r\n{0}```'.format("".join(traceback.TracebackException.from_exception(e).format())), inline=False)
+            msg_embed.set_author(name=str(translator.translate('embed_title', 'eval', inst.language)))
+            msg_embed.add_field(translator.translate('embed_fields', 'eval_resulf', inst.language), '```py\r\n{0}```'.format("".join(traceback.TracebackException.from_exception(e).format())), inline=False)
     else:
         msg_embed = disnake.Embed(
-            description=str(translator.translate('embed_description', 'forbidden', language)),
+            description=str(translator.translate('embed_description', 'forbidden', inst.language)),
             colour=config['accent_err']
         )
-        msg_embed.set_author(name=str(translator.translate('embed_title', 'forbidden', language)))
+        msg_embed.set_author(name=str(translator.translate('embed_title', 'forbidden', inst.language)))
     return msg_embed
 
-async def sendRegularMsg(ctx, bot, config, language, disnake, translator, arg):
-    msg_embed = await generateEmbed(ctx, bot, config, language, disnake, translator, arg)
+async def sendRegularMsg(ctx, inst, config, disnake, translator, arg):
+    msg_embed = await generateEmbed(ctx, inst, config, disnake, translator, arg)
     await ctx.reply(embed=msg_embed, mention_author=False)
 
 async def sendGoodbyeMsg(ctx, inst, config, disnake, translator):
