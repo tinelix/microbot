21b015197328a09be048d3f78ab8f7cfd1d8e997
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-01-01 19:23:43 +0700

    Update timers.py

--- discord-api/Commands/timers.py
+++ discord-api/Commands/timers.py
@@ -52,7 +52,7 @@ async def generateEmbed(ctx, inst, config, disnake, translator, arg, db):
                     colour=config['accent_err'],
                     description='{0}\r\n{1}'.format(translator.translate('embed_description', 'invalid_cmd_usage', inst.language), translator.translate('command_examples', 'timers_create', inst.language).format(config['prefix']))
                 )
-                msg_embed.set_author(name=translator.translate('embed_title', 'timers', language))
+                msg_embed.set_author(name=translator.translate('embed_title', 'timers', inst.language))
         elif(args[0] == '-Ce'):
             try:
                 timer_name = re.search('-Ce (.+?) -t', arg).group(1)
