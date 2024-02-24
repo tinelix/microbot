db724c43a014d106dab3633786594792967fc0ad
Dmitry Tretyakov <76806170+tretdm@users.noreply.github.com>
2024-02-24 20:01:50 +0700

    Add an explanation for choosing a software license

--- discord-api/Utilities/setupwiz.py
+++ discord-api/Utilities/setupwiz.py
@@ -15,23 +15,24 @@
 
 def gen_config_file(sys, config, links):
     if 'license' not in config or config['license'] == 0:
-        print("""Before the first launch of Microbot, you must select a license to distribute and create derivative works
- (for example, source code based Microbot modifications).
+        print(""" Before the first launch of Microbot, you must select a license to distribute and create derivative works
+ (for example, Microbot modifications).
 
  \033[92m1. Apache License 2.0 & Affero GPLv3 or later version (recommended)
  \033[0m   Requires public release of modified source code (as with GPLv3) and gives developers the same choice.
 
  2. Affero GPLv3 or later version
-    Requires the public release of modifed source code.
+    This option guarantees all users full freedom of action under equal terms. Requires the public release of
+    modified source code.
 
  3. Apache License 2.0
-    Useful mainly for beginner or inexperienced developers. Anyone can view the source code and create a
-    derivative work without worrying about changes to the source code.
+    Useful mainly for beginners or inexperienced developers. Anyone can use this work as a template for drawing
+    up an initial development concept without worrying about changes to the source code.
 """)
         license_choice = int(input(" Your choice (1-3): "))
-        print(" Choosed: {0}".format(license_choice))
-        with open("config.py", "w") as f:
-            f.write(f"""
+        if(license_choice == 1 or license_choice == 2 or license_choice == 3):
+            with open("config.py", "w") as f:
+                f.write(f"""
 # Microbot Discord bot
 # Generated 'config.py' file
 
@@ -62,5 +63,8 @@ links = {{
     'repo': '{links['repo'] if('repo' in links) else ''}',
     'donate': '{links['donate'] if('donate' in links) else ''}'
 }}
-            """.replace("\t",""))
+                """.replace("\t",""))
             f.close()
+        else:
+            print("\r\n Invalid choice. Please try again.")
+            sys.exit()
